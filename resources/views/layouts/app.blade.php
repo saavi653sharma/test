55777bd0060f8d3c63322ca2ce6026af91becf26
savita <savitacbl24@gmail.com>
2025-05-06 12:04:55 +0530

    add line

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
             @yield('content')
         </main>
     </div>
-
+hi this is the content of branch first
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     @stack('scripts')
